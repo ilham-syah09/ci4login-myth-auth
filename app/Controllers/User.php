2c3c6767eee7213149ae05337fd759f961dde2b5
ilham-syah09 <ilham.xavi44@gmail.com>
2021-09-10 17:09:55 +0700

    add data sensor

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,11 +2,39 @@
 
 namespace App\Controllers;
 
+use App\Models\DataModel;
+
+
 class User extends BaseController
 {
+    protected $dataModel;
+
+    public function __construct()
+    {
+        $this->dataModel = new DataModel();
+    }
     public function index()
     {
         $data['title'] = 'My Profile';
         return view('user/index', $data);
+    }
+
+    public function data()
+    {
+        $sensor = $this->dataModel->findAll();
+
+        $data = [
+            'title' => 'Data Sensor',
+            'data'  => $sensor
+        ];
+        return view('user/data', $data);
+    }
+
+    public function simpan()
+    {
+        $this->dataModel->save([
+            'value' => $this->request->getVar('value')
+        ]);
+        echo 'sukses insert data';
     }
 }
